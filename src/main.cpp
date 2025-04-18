c78755e2f60af6594df0500c1a650d7344300217
Allan K. Koech <allankoech.ke@gmail.com>
2025-04-18 14:56:49 +0300

    Initial app commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,20 @@ int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
 
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
+    // Handle window closing ...
+    ui->on_quit([&] -> void { ui->window().dispatch_close_requested_event(); });
+
+    // Handle minimize window action
+    ui->on_window_minimized([&](const bool enabled) -> void { ui->window().set_minimized(enabled); });
+
+    // Handle minimize window action
+    ui->on_window_maximized([&](const bool enabled) -> void { ui->window().set_maximized(enabled); });
+
+    ui->on_window_moved([&](const double &dx, const double &dy) -> void
+    {
+        const auto physical_pos = ui->window().position();
+        const auto new_pt = slint::Point<int32_t>(physical_pos.x + dx, physical_pos.y + dy);
+        ui->window().set_position(slint::PhysicalPosition(new_pt));
     });
 
     ui->run();
